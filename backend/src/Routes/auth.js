2b51d02058dab3be2e4643fb982b1abefe86d362
ayaan <ayaanshaikh09092@gmail.com>
2026-06-15 12:41:45 +0530

    Fix import path casing for auth middleware in auth.js

--- backend/src/Routes/auth.js
+++ backend/src/Routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../Models/userModel.js"
-import { protect } from "../middleware/auth.js";
+import { protect } from "../Middleware/auth.js";
 import { generateToken,signup,login,logout,updateProfile,getCurrentUser } from "../Controllers/index.js";
 const router = express.Router();
 
