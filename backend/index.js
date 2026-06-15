32ca5f7572bfb4f09874d332b81492e54ce820db
ayaan <ayaanshaikh09092@gmail.com>
2026-06-15 11:54:44 +0530

    added docker dockerignore and some code in index.js for the production

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,29 @@ import dns from "dns";
 import authRouter from "./src/Routes/auth.js";
 import userRouter from "./src/Routes/user.js";
 import cookieParser from "cookie-parser";
+import path from 'path'
+import fs from 'fs'
 dotenv.config();
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT;
-
+const FRONTEND_URL=process.env.FRONTEND_URL;
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
 app.use("/api/auth", authRouter);
 app.use("/api/user",userRouter);
 
+const publicDir = path.join(process.cwd(),'public')
+
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir));
+  app.get('/{*any}',(req,res,next)=>{
+    res.sendFile(path.join(publicDir,'index.html'),(err)=>next(err))
+  })
+}
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
